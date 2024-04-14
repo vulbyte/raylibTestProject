4b03e7ec0cbf127854febf9ed54afee2a983e466
vulbyte <vulbyt3@gmail.com>
2024-04-14 15:14:36 -0700

    added git ignore

--- example.jsx
+++
@@ -1,7 +0,0 @@
-export default function textField() {
-    const [text, setText] = useRef("");
-    
-    return (
-        <input placeholder="text here!" id="textField" onChange={setText(val)}/>
-    );
-}
